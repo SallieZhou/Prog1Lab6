30f0d870366e8c91aa03cead2cb6e65f5a420c2a
jonana-split <jmijares03@hotmail.com>
2023-03-21 23:38:44 -0400

    I've finished the decode function and main function.

--- githublab6sallie.py
+++ githublab6sallie.py
@@ -1,5 +1,7 @@
 # Sallie Zhou
 
+# Lab Partner is Joanna Mijares
+
 def encode(password):
     encoded_password = []
     final_encode = ""
@@ -10,8 +12,20 @@ def encode(password):
     return final_encode
 
 
+def decode(final_encode):
+    decoded_password = []
+    final_decode = ""
+    for digit in final_encode:
+        decoded_digit = int(digit) - 3
+        decoded_password.append(decoded_digit)
+    final_decode = "".join(str(digit) for digit in decoded_password)
+    return final_decode
+
+
 a = True
+
 while a:
+
     print("Menu")
     print("-------------")
     print("1. Encode")
@@ -19,6 +33,16 @@ while a:
     print("3. Quit")
     print()
     menu_option = int(input("Please enter an option: "))
+
     if menu_option == 1:
         password = input("Please enter your password to encode: ")
-        encode(password)
+        final_encode = encode(password)
+        print()
+
+    elif menu_option == 2:
+        final_decode = decode(final_encode)
+        print(f"The encoded password is {final_encode}, and the original password is {final_decode}.")
+        print()
+
+    elif menu_option == 3:
+        break
